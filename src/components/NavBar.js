e9147cdc6b20284dfe91284fbb6e173ca78bbe0f
Kurtis <kcd257@yahoo.com>
2022-04-19 12:08:59 -0700

    update

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,4 +1,4 @@
-import Navbar from 'react-bootstrap/NavBar'
+import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
 import Container from 'react-bootstrap/Container'
 import { BsTelephoneFill } from "react-icons/bs";
